590a02f778270561b140de5d41a0168d88233c03
meisimo <siruasor@gmail.com>
2021-05-19 22:39:56 -0500

    shoot

---
+++ Assets/Scripts/MainCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour
+{
+
+    private Animator animator;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            animator.SetTrigger("Fire");
+        }
+    }
+}
